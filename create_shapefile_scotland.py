3df308500d15fdf1726b8cf3360687d49960ad48
marzbanf <153090439+marzbanf@users.noreply.github.com>
2023-12-13 05:49:44 +0100

    Update create_shapefile_scotland.py

--- create_shapefile_scotland.py
+++ create_shapefile_scotland.py
@@ -1,25 +1,26 @@
+from socket import INADDR_ANY
 import geopandas as gpd
 import os
 from shapefile_utils import get_largest_geometry
 
 ROOT = os.getcwd()
-COUNTRIES = f'{ROOT}/country_data/country_region.shp'
-SCOTLAND_FULL = f'{ROOT}/country_data/scotland_full.shp'
-SCOTLAND = f'{ROOT}/country_data/scotland.shp'
+COUNTRIES = f'{ROOT}/kermanshah_data/kermanshah_region.shp'
+KERMANSHAH_FULL = f'{ROOT}/kermanshah_data/kermanshah_full.shp'
+KERMANSHAH = f'{ROOT}/kermanshah_data/kermanshah.shp'
 EPSG = "EPSG:4326"
 
-# Countries UK
+# Countries Iran
 gdf_country = gpd.read_file(COUNTRIES)
 # Filter only for SCT
-coastline_sct = gdf_country[gdf_country.NAME=='Scotland']
-# Save Scotland boundaries as shapefile
-coastline_sct.to_file(SCOTLAND_FULL, driver='ESRI Shapefile')
-print("Scotland shapefile (full, with islands) saved")
+coastline_sct = gdf_country[gdf_country.NAME=='Kermanshah']
+# Save Kermanshah boundaries as shapefile
+coastline_sct.to_file(KERMANSHAH_FULL, driver='ESRI Shapefile')
+print("Kermanshah shapefile (full, with kermanshah) saved")
 
-# Read the shapefile for Scotland (full, mainland + islands)
-scotland_shapefile = gpd.read_file(SCOTLAND_FULL)
-# Get the largest geometry from the scotland shapefile (thus, the mainland)
+# Read the shapefile for Kermanshah (full, mainland + kermanshah)
+kermanshah_shapefile = gpd.read_file(KERMANSHAH_FULL)
+# Get the largest geometry from the kermanshah shapefile (thus, the mainland)
 # And convert it to CRS common format
-sct_inland = get_largest_geometry(scotland_shapefile).to_crs(EPSG)
-sct_inland.to_file(SCOTLAND, driver='ESRI Shapefile')
-print("Scotland shapefile (mainland only) saved")
+sct_kermanshah = get_largest_geometry(kermanshah_shapefile).to_crs(EPSG)
+sct_kermanshah.to_file(KERMANSHAH, driver='ESRI Shapefile')
+print("Kermanshah shapefile (mainland only) saved")
